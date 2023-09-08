f554a267c63a9c433ed5db05704d2ee7cc8d2c94
gyureal <yonggyu.jeong10@gmail.com>
2023-09-08 22:12:42 +0900

    Initial commit

---
+++ src/main/java/com/example/codereviewstudy/CodeReviewStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.codereviewstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReviewStudyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodeReviewStudyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/codereviewstudy/CodeReviewStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codereviewstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeReviewStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
